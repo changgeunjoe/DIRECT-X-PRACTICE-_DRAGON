7707f14ffdca3bc32c8ca6cd2d5ac553686f6095
changgeunjoe <hoi000115@gmail.com>
2022-05-20 23:44:38 +0900

    따라하기 8직육면체 완료

--- stdafx.h
+++ stdafx.h
@@ -4,7 +4,7 @@
 #define _WITH_SWAPCHAIN_FULLSCREEN_TRUE
 #pragma once
 
-
+#define RANDOM_COLOR XMFLOAT4(rand()/float(RAND_MAX),rand()/float(RAND_MAX),rand()/float(RAND_MAX),rand()/float(RAND_MAX))
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일
